c2bec73c582fd0d8ee13f6b5603022645c20db6f
morrison-ben <morrisonb84@gmail.com>
2020-10-16 23:26:55 -0400

    more game details

--- client/src/components/game/game.js
+++ client/src/components/game/game.js
@@ -2,13 +2,53 @@ import React, { Component } from 'react';
 
 class Game extends Component {
 
+
+  gameState = this.props.status.type.state;
+
+
   render() {
-    return (
-      <div className='game-box'>
-        <h2>{this.props.name}</h2>
-        <p>{this.props.date}</p>
-      </div>
-    )
+
+    //PREGAME
+    if (this.gameState === "pre") {
+      return (
+        <div className='game-box'>
+
+          <h2>{this.props.name}</h2>
+          <p>{this.props.date}</p>
+
+          {/*home abbreviation */}
+          <p>{this.props.competitions[0].competitors[0].team.abbreviation}</p>
+          <p>{this.props.competitions[0].competitors[0].team.displayName}</p>
+
+          {/*home record */}
+          <p>{this.props.competitions[0].competitors[0].records[0].summary}</p>
+
+          <img src={this.props.competitions[0].competitors[0].team.logo} alt="logo" />
+
+
+        </div>
+      )
+    }
+
+    //LIVE
+    if (this.gameState === "in") {
+      return (
+        <div className='game-box'>
+          <h2>{this.props.name}</h2>
+          <p>{this.props.date}</p>
+          <p>{this.gameState}</p>
+        </div>
+      )
+    }
+
+
+    //POSTGAME
+    if (this.gameState === "post") {
+      return (
+        <div></div>
+      )
+    }
+
   }
 }
 
